f6af2ea9b1f31674580fa48b37f927eb552ee94f
przlovegithub <prz1991412@126.com>
2018-08-09 17:33:56 +0800

    添加全局钩子函数

--- src/router/index.js
+++ src/router/index.js
@@ -7,18 +7,50 @@ const score = r => require.ensure([], () => r(require('@/components/score')), 's
 Vue.use(VueRouter)
 
 // export default new Router({
-export default new VueRouter({
+const router = new VueRouter({
     routes: [{
             path: '/',
             name: 'choces',
-            component: choces
+            component: choces,
+            meta: {
+                title: '问题'
+            }
         },
         {
             path: '/score',
             name: 'score',
-            component: score
+            component: score,
+            meta: {
+                title: '分数'
+            }
         }
 
     ]
 
 })
+router.beforeEach((to, from, next) => { //全局钩子函数
+    // console.log(to);
+    let documentTitle = '一站到底';
+    // path 是多级的，遍历
+    to.matched.forEach((path) => { //动态修改文档标题
+        // console.log(path);
+        if (path.meta.title) {
+            documentTitle += ` - ${path.meta.title}`;
+        }
+    });
+    document.title = documentTitle;
+    to.matched.some((path) => {
+        // to.matched.forEach((path) => {
+        if (path.meta.need2Login) { //通过此操作可以判断哪些页面需要登录
+            if (localStorage.token) {
+                next()
+            } else {
+                next({ path: '/login' })
+            }
+        } else {
+            next();
+        }
+    });
+
+});
+export default router;
